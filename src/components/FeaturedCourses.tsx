2de27bdc6bb2a897f0c631776f301d044901006b
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-01-05 17:58:45 +0530

    Add Instructors component and AnimatedTooltip; update FeaturedCourses to use local course data and remove courses.json

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -1,18 +1,69 @@
 "use client";
-import Link from "next/link";
-import courseData from "@/courses.json";
 import { BackgroundGradient } from "./ui/BackgroundGradient";
 import Image from "next/image";
-import PrimaryButton from "./PrimaryButton";
 
-interface Course {
-  id: number;
-  title: string;
-  description: string;
-  image: string;
-  price: number;
-  instructor: string;
-}
+const courses = [
+  {
+    id: 1,
+    title: "Piano for Beginners",
+    description:
+      "Start your musical journey with foundational piano skills taught by expert instructors.",
+    price: 109.99,
+    instructor: "Jane Smith",
+    image:
+      "https://res.cloudinary.com/mohammadbilalmansuri/image/upload/v1736048427/musicacademy/piano.webp",
+  },
+  {
+    id: 2,
+    title: "Guitar Essentials",
+    description:
+      "Learn the basics of guitar playing, including chords, strumming, and simple songs.",
+    price: 99.99,
+    instructor: "John Doe",
+    image:
+      "https://res.cloudinary.com/mohammadbilalmansuri/image/upload/v1736048426/musicacademy/guitar.webp",
+  },
+  {
+    id: 3,
+    title: "Violin Mastery",
+    description:
+      "Master the violin with advanced techniques and repertoire for intermediate players.",
+    price: 129.99,
+    instructor: "Emily Davis",
+    image:
+      "https://res.cloudinary.com/mohammadbilalmansuri/image/upload/v1736048427/musicacademy/violin.webp",
+  },
+  {
+    id: 4,
+    title: "Drumming Fundamentals",
+    description:
+      "Develop your rhythm and timing with comprehensive drum lessons for all levels.",
+    price: 89.99,
+    instructor: "Michael Brown",
+    image:
+      "https://res.cloudinary.com/mohammadbilalmansuri/image/upload/v1736048426/musicacademy/drum.webp",
+  },
+  {
+    id: 5,
+    title: "Flute Techniques",
+    description:
+      "Enhance your flute playing skills with lessons on breath control and articulation.",
+    price: 119.99,
+    instructor: "Sophia Wilson",
+    image:
+      "https://res.cloudinary.com/mohammadbilalmansuri/image/upload/v1736048426/musicacademy/flute.webp",
+  },
+  {
+    id: 6,
+    title: "Clarinet Essentials",
+    description:
+      "Learn the fundamentals of clarinet playing, including tone production and fingerings.",
+    price: 89.99,
+    instructor: "Liam Anderson",
+    image:
+      "https://res.cloudinary.com/mohammadbilalmansuri/image/upload/v1736048426/musicacademy/clarinet.webp",
+  },
+];
 
 export default function FeaturedCourses() {
   return (
@@ -29,7 +80,7 @@ export default function FeaturedCourses() {
         </div>
 
         <div className="w-full grid grid-cols-3 gap-8 justify-center">
-          {courseData.map((course: Course, index) => (
+          {courses.map((course, index) => (
             <BackgroundGradient
               key={index}
               className="size-full flex flex-col items-start gap-5 rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden p-8"
---
+++ src/components/Instructors.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { AnimatedTooltip } from "./ui/AnimatedTooltip";
+import { WavyBackground } from "./ui/WavyBackground";
+
+const people = [
+  {
+    name: "John Smith",
+    designation: "Piano Expert",
+    image:
+      "https://res.cloudinary.com/mohammadbilalmansuri/image/upload/v1736079227/musicacademy/john-smith.webp",
+  },
+  {
+    name: "John Doe",
+    designation: "Guitar Expert",
+    image:
+      "https://res.cloudinary.com/mohammadbilalmansuri/image/upload/v1736079227/musicacademy/john-doe.webp",
+  },
+  {
+    name: "Emily Davis",
+    designation: "Violin Expert",
+    image:
+      "https://res.cloudinary.com/mohammadbilalmansuri/image/upload/v1736079227/musicacademy/emily-davis.webp",
+  },
+  {
+    name: "Michael Brown",
+    designation: "Drum Expert",
+    image:
+      "https://res.cloudinary.com/mohammadbilalmansuri/image/upload/v1736079227/musicacademy/mitchel-brown.webp",
+  },
+  {
+    name: "Sophia Wilson",
+    designation: "Flute Expert",
+    image:
+      "https://res.cloudinary.com/mohammadbilalmansuri/image/upload/v1736079228/musicacademy/sophia-wilson.webp",
+  },
+  {
+    name: "Liam Anderson",
+    designation: "Clarinet Expert",
+    image:
+      "https://res.cloudinary.com/mohammadbilalmansuri/image/upload/v1736079227/musicacademy/emily-davis.webp",
+  },
+];
+
+export default function Instructors() {
+  return (
+    <section className="w-full relative overflow-hidden">
+      <WavyBackground className="w-full relative flex flex-col items-center justify-center gap-10 px-5 py-24">
+        <div className="flex flex-col items-center gap-5">
+          <h2 className="bg-clip-text text-transparent text-center bg-white text-xl md:text-3xl lg:text-6xl font-bold tracking-tight">
+            Meet Our Instructors
+          </h2>
+          <p className="text-2xl">
+            Discover the talented professionals who will guide your musical
+            journey
+          </p>
+        </div>
+
+        <div className="flex items-center">
+          <AnimatedTooltip items={people} />
+        </div>
+      </WavyBackground>
+    </section>
+  );
+}
