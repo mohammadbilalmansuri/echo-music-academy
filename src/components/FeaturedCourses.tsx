7762d0cf7ad6d88bfb5a29af904623533ffde473
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-01-05 10:32:09 +0530

    Add WhyChooseUs component and StickyScroll functionality; update FeaturedCourses layout and styles

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -23,14 +23,14 @@ export default function FeaturedCourses() {
 
   return (
     <section className="w-full relative flex flex-col items-center">
-      <div className="w-full max-w-screen-2xl relative flex flex-col items-center gap-16 px-4 py-20">
+      <div className="w-full max-w-screen-2xl relative flex flex-col items-center gap-20 px-4 py-24">
         <div className="flex flex-col items-center gap-5">
+          <p className="text-2xl text-white/60">Featured Courses</p>
           <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-white/20 to-white text-xl md:text-3xl lg:text-6xl font-bold tracking-tight">
-            Featured Courses
+            Learn with the best
           </h2>
-          <p className="text-2xl text-white/60">Learn with the best</p>
         </div>
-        <div className="w-full grid grid-cols-3 gap-10 justify-center">
+        <div className="w-full grid grid-cols-3 gap-10 justify-center mb-5">
           {featuredCourses.map((course: Course, index) => (
             <BackgroundGradient
               key={index}
@@ -50,7 +50,7 @@ export default function FeaturedCourses() {
             </BackgroundGradient>
           ))}
         </div>
-        <PrimaryButton>View All Courses</PrimaryButton>
+        <PrimaryButton href="/courses">View All Courses</PrimaryButton>
       </div>
     </section>
   );
