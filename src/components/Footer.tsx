c315e57088b3e8b1954c8d646200b54581b17556
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-02-13 07:49:50 +0530

    make header, footer reasponsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,25 @@
 "use client";
-export default function Footer() {
+const Footer = () => {
   return (
     <footer
       id="contact-us"
       className="w-full relative flex flex-col items-center bg-dot-white/15"
     >
-      <div className="w-full max-w-screen-2xl relative flex flex-col items-center text-center gap-10 px-5 pt-24 pb-8">
+      <div className="w-full max-w-screen-2xl relative flex flex-col items-center text-center gap-10 px-5 xl:pt-24 lg:pt-20 sm:pt-16 pt-12 pb-8">
         <div className="flex flex-col items-center gap-5">
-          <h2 className="text-3xl font-medium">About Us</h2>
-          <p className="text-lg text-white/60 max-w-2xl">
+          <h2 className="md:text-3xl text-2xl font-medium">About Us</h2>
+          <p className="md:text-lg text-white/60 max-w-2xl">
             Echo is a premier institution dedicated to teaching the art and
             science of music. We nurture talent from the ground up, fostering a
             vibrant community of musicians.
           </p>
 
-          <div className="flex items-center gap-3 mt-3">
+          <div className="flex items-center xs:gap-3 gap-2 mt-3">
             <a
               href="#"
               className="bg-white/10 hover:bg-white/20 p-3 rounded-full fill-white"
             >
-              <svg viewBox="0 0 576 512" className="size-5">
+              <svg viewBox="0 0 576 512" className="xs:size-5 size-4">
                 <path d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
               </svg>
             </a>
@@ -27,7 +27,7 @@ export default function Footer() {
               href="#"
               className="bg-white/10 hover:bg-white/20 p-3 rounded-full fill-white"
             >
-              <svg viewBox="0 0 512 512" className="size-5">
+              <svg viewBox="0 0 512 512" className="xs:size-5 size-4">
                 <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
               </svg>
             </a>
@@ -35,7 +35,7 @@ export default function Footer() {
               href="#"
               className="bg-white/10 hover:bg-white/20 p-3 rounded-full fill-white"
             >
-              <svg viewBox="0 0 512 512" className="size-5">
+              <svg viewBox="0 0 512 512" className="xs:size-5 size-4">
                 <path d="M64 112c-8.8 0-16 7.2-16 16l0 22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1l0-22.1c0-8.8-7.2-16-16-16L64 112zM48 212.2L48 384c0 8.8 7.2 16 16 16l384 0c8.8 0 16-7.2 16-16l0-171.8L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64l384 0c35.3 0 64 28.7 64 64l0 256c0 35.3-28.7 64-64 64L64 448c-35.3 0-64-28.7-64-64L0 128z" />
               </svg>
             </a>
@@ -43,7 +43,7 @@ export default function Footer() {
               href="#"
               className="bg-white/10 hover:bg-white/20 p-3 rounded-full fill-white"
             >
-              <svg viewBox="0 0 320 512" className="size-5">
+              <svg viewBox="0 0 320 512" className="xs:size-5 size-4">
                 <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
               </svg>
             </a>
@@ -51,7 +51,7 @@ export default function Footer() {
               href="#"
               className="bg-white/10 hover:bg-white/20 p-3 rounded-full fill-white"
             >
-              <svg viewBox="0 0 512 512" className="size-5">
+              <svg viewBox="0 0 512 512" className="xs:size-5 size-4">
                 <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
               </svg>
             </a>
@@ -59,7 +59,7 @@ export default function Footer() {
               href="#"
               className="bg-white/10 hover:bg-white/20 p-3 rounded-full fill-white"
             >
-              <svg viewBox="0 0 448 512" className="size-5">
+              <svg viewBox="0 0 448 512" className="xs:size-5 size-4">
                 <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
               </svg>
             </a>
@@ -67,9 +67,11 @@ export default function Footer() {
         </div>
 
         <p className="text-white/60 border-t border-white/20 pt-7">
-          © 2024 Echo Music Academy. All rights reserved.
+          ©2024 Echo Music Academy. Created by Mohammad Bilal Mansuri.
         </p>
       </div>
     </footer>
   );
-}
+};
+
+export default Footer;
